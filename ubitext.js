f9ac5a77588cfa2c7aaf83170f02845996dc8eb2
Guilherme Guerra <guerrinha@comum.org>
2016-03-04 07:42:54 -0300

    week 2

--- ubitext.js
+++ ubitext.js
@@ -1,9 +1,9 @@
 this.Documents = new Mongo.Collection("documents");
-
+EditingUsers = new Mongo.Collection("editingUsers");
 
 if (Meteor.isClient) {
 
-  
+
   Template.editor.helpers({
     docid: function() {
       var doc = Documents.findOne();
@@ -16,9 +16,12 @@ if (Meteor.isClient) {
     },
     config: function() {
       return function(editor) {
+        editor.setOption("lineNumbers", true);
+        editor.setOption("theme", "cobalt");
         editor.on("change", function(cm_editor, info) {
           $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
-        })
+          Meteor.call("addEditingUser");
+        });
       }
     }
   });
@@ -30,6 +33,23 @@ if (Meteor.isServer) {
     if (!Documents.findOne()) { // no documents yet!
       Documents.insert({title: "Sample document"});
     }
-    
+
   });
 }
+
+Meteor.methods({
+  addEditingUser: function() {
+    var doc, user, eusers;
+    if(!doc) {return;} // give up
+    if(!this.userId){return;} // no user logged
+
+    user = Meteor.user().profile;
+    eusers = EditingUsers.findOne({docid:doc._id});
+    if(!eusers){
+      eusers = {
+        docid: doc._id,
+      };
+    }
+    EditingUsers.insert({eusers});
+  }
+});
