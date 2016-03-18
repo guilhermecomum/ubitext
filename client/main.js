502de72de8cecea9dfbd4dcb0ab4495088d2cf7a
Guilherme Guerra <guerrinha@comum.org>
2016-03-18 10:52:58 -0300

    Adding iron router and split files

--- client/main.js
+++ client/main.js
@@ -1,6 +1,25 @@
 Meteor.subscribe("documents");
 Meteor.subscribe("editingUsers");
 
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function(){
+  this.render("navbar", {to: "header"});
+  this.render("docList", {to: "main"});
+});
+
+Router.route('/documents/:_id', function(){
+  console.log("you hit /documents " + this.params._id);
+  Session.set("docid", this.params._id);
+  this.render("navbar", {to: "header"});
+  this.render("docItem", {to: "main"});
+});
+
+
+
+
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
@@ -73,6 +92,12 @@ Template.editableText.helpers({
     } else {
       return false;
     }
+  }
+});
+
+Template.docList.helpers({
+  documents: function() {
+    return Documents.find();
   }
 });
 
